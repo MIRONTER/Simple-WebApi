b72d110191f72694154b8ee706e3c11f48f093d4
MIRONTER <91906992+MIRONTER@users.noreply.github.com>
2023-02-12 08:48:51 +0300

    Add models and context

---
+++ WebApi/WebApi/Models/Result.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApi.Models;
+
+public partial class Result
+{
+    public DateTime AllTime { get; set; }
+
+    public DateTime MinDate { get; set; }
+
+    public double AverageSeconds { get; set; }
+
+    public double AverageN { get; set; }
+
+    public double MedianN { get; set; }
+
+    public int MaxN { get; set; }
+
+    public int MinN { get; set; }
+
+    public int Count { get; set; }
+}
---
+++ WebApi/WebApi/Models/Value.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApi.Models;
+
+public partial class Value
+{
+    public int Id { get; set; }
+
+    public string FileName { get; set; } = null!;
+
+    public DateTime Date { get; set; }
+
+    public int Seconds { get; set; }
+
+    public double N { get; set; }
+}
